66ce56a5269012ff7da4ba75eb2cf07d1c69498c
Ifihan <victoriaolusheye@gmail.com>
2021-05-10 00:14:46 +0100

    Fixed whitenoise issue

--- crm/settings.py
+++ crm/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
