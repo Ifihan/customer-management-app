d8a2a3292a32c952c755c57e3dc448c4065a5976
Ifihan <victoriaolusheye@gmail.com>
2021-05-10 00:19:52 +0100

    Made changes

--- crm/settings.py
+++ crm/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ifihan-cma.herokuapp.com', '127.0.0.1']
 
