1759ad7d0210dfa2a3373723cebde36f40616cb8
Ifihan <victoriaolusheye@gmail.com>
2021-05-10 14:24:15 +0100

    Fixed login

--- accounts/views.py
+++ accounts/views.py
@@ -22,7 +22,7 @@ def registerPage(request):
             user = form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, "Account sucessfully created for " + username)
-            return redirect('login')
+            return redirect('login/')
 
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
