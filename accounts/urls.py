7ebd79703c1bef73284e9719627b8b07e3551a96
Ifihan <victoriaolusheye@gmail.com>
2021-05-11 16:33:11 +0100

    Removed admin url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,6 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
 
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
