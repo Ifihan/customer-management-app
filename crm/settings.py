2d159f34c79847920c637cb88e2e570b480b972a
K-Honsu <119008867+K-Honsu@users.noreply.github.com>
2024-02-16 18:47:13 +0100

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -152,6 +152,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'victoriaolusheye@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
